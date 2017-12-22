32f122fd2f94fe2d41dd0aa79925e693429448c2
John Thomason <johnsthomason@gmail.com>
2017-12-21 19:29:21 -0800

    adding Linux-Info-2.sh

---
+++ Linux-Info-2.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#
+# Linux Shell Scripting Task #2
+# John Thomason
+#
+
+# Functions
+
+
+# Title
+echo -e "\nLinux Shell Scripting Task #2"
+
+#Please verify the below items based on input:
+#
+# List the files installed by specific package
+
+# Identify the package which installed the specific file in system
+
+# If user/group name provided, please check the user or group exist
+
+# Kill any user provided process if it exists
+
+# Create cron job using shell script
+
+
